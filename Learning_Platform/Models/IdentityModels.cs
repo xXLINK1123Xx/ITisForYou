488d178d49b5ab04a0a690ea2529362e7f9bcedb
Sasha <sasha.yaroshepta@gmail.com>
2019-06-21 23:01:15 +0300

    FIx user context con string

--- Learning_Platform/Models/IdentityModels.cs
+++ Learning_Platform/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Learning_Platform.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("LPDataContext", throwIfV1Schema: false)
         {
         }
         
