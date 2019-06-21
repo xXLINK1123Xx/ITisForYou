a08730d08fb36494f5623f480c8a74f48058fb6e
AndrijDen <andrij.denysenko@gmail.com>
2019-06-21 23:15:10 +0300

    dto

--- Learning_Platform/Controllers/CourseController.cs
+++ Learning_Platform/Controllers/CourseController.cs
@@ -63,6 +63,12 @@ namespace Learning_Platform.Controllers
         public class CourseDto
         {
             public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public string Image { get; set; }
         }
 
         public class Mapping : Profile
